d0515c31d93def747b2aedb4bd16002ba5a58670
Juan Esparza <juanj.esparza@gmail.com>
2017-08-12 21:36:41 -0700

    Added brew_install.sh to the list of files to exclude

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,7 +5,7 @@ cd "$(dirname "${BASH_SOURCE}")";
 # git pull origin master;
 
 function doIt() {
-	rsync --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh" --exclude "not-sync/" \
+	rsync --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh" --exclude "not-sync/" --exclude "brew_install.sh" \
 		--exclude "README.md" --exclude "LICENSE-MIT.txt" -avh --no-perms . ~;
 
 	source ~/.bash_profile;
